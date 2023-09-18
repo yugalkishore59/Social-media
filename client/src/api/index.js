4f35972933c7445597829b2ca2efbb271a150c5d
Yugal Kishore <yugalkishore59@gmail.com>
2023-09-19 05:23:52 +0530

    completed backend
    add state management using zustand
    implemented fetch and put posts on mogodb

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,3 +3,4 @@ import axios from 'axios'
 const url = 'http://localhost:3002/posts';
 
 export const fetchPosts = ()=> axios.get(url);
+export const createPosts = (newPost)=> axios.post(url, newPost);
