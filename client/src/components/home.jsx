fd5d8ab5ce0d4a076a5cb5bc58ec4f9936844aa6
Yugal Kishore <yugalkishore59@gmail.com>
2023-09-27 16:08:58 +0530

    added react dom router

---
+++ client/src/components/home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Posts from "./Posts/posts";
+import Form from "./form";
+import usePostStore from "../store/post";
+
+function Home() {
+    const refreshPosts = usePostStore((state) => state.getPosts)();
+  return (
+    <div>
+        
+        <div className="app_body">
+            <div className="left_body">Left</div>
+            <div className="main_body">
+            <Form />
+            <Posts />
+            </div>
+            <div className="right_body">Right</div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
